fead832cf544c9a89fd2bf28a0945c9888822324
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-07-28 20:03:04 +0700

    menambahkan variabel

--- variablel.go
+++ variablel.go
@@ -4,8 +4,27 @@
  
  func main()  {
 	var firstName string ="Haikal "
-	var lastName string = "Frastiawan"
+	const lastName string = "Frastiawan"
 	fmt.Print("hallo ", firstName, lastName)
+
+	//tipe data
+	var bilanganBulat uint8 = 20
+	var decimalNumber = 3.60
+	var tipeBool = true
+
+
+	fmt.Print("\n ","umur saya: ", bilanganBulat)
+	fmt.Print("\n ","Ipk saya:", decimalNumber)
+	fmt.Print("\n ","Saya ganteng: ", tipeBool)
+
+	//variabel pointer
+	var numberA int = 4
+	var numberB *int = &numberA
 	
+	fmt.Print("\nnumber A (value) : ", numberA)
+	fmt.Print("\nnumber A (address) : ", &numberA)
+	fmt.Print("\nnumber B (value) : ", numberB)
+	fmt.Print("\nnumber B (address) : ", &numberB)
+
 
  }
