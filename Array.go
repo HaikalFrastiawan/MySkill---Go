97ce51810ac85f5b957b892aea2e984c4c69aa5b
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-07-31 05:43:47 +0700

    menambahkan file array

---
+++ Array.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	var fruits = [3]string 	{"apel", "mangga", "piscyang"}
+	fmt.Println("jumlah isi element \t\t" , len(fruits))
+	fmt.Println("isi semua element \t\t",fruits)
+}
